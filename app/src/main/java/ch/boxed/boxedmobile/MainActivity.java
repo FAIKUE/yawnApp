46d2170604b9df275390a6f15ee2c6ab9b46a7b3
Fabian Künzle <fabian.kuenzle@gmail.com>
2015-11-07 23:58:43 +0100

    Profile Action hinzugefügt

--- app/src/main/java/ch/boxed/boxedmobile/MainActivity.java
+++ app/src/main/java/ch/boxed/boxedmobile/MainActivity.java
@@ -1,7 +1,13 @@
 package ch.boxed.boxedmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user clicks the add Task Button */
+    public void addTask(View view){
+        Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    /** Called when the user clicks the I'M BORED Button */
+    public void showTask(View view){
+
+    }
+
 }
